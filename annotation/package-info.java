6c2945212ce089aada36f167219a38d86563adda
akashyelisetty <43619353+akashyelisetty@users.noreply.github.com>
2019-06-19 09:47:24 -0400

    Add files via upload

--- annotation/package-info.java
+++ annotation/package-info.java
@@ -1,9 +1,9 @@
 /**
- * Support package for beans-style handling of Java 5 annotations.
+ * Support package for annotation-driven bean configuration.
  */
 @NonNullApi
 @NonNullFields
-package org.springframework.beans.annotation;
+package org.springframework.beans.factory.annotation;
 
 import org.springframework.lang.NonNullApi;
 import org.springframework.lang.NonNullFields;
