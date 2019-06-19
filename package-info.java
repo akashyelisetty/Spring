f18cc62bf140a5a8513a117f82efaff37f28a710
akashyelisetty <43619353+akashyelisetty@users.noreply.github.com>
2019-06-19 09:45:23 -0400

    Add files via upload

--- package-info.java
+++ package-info.java
@@ -1,17 +1,17 @@
 /**
- * This package contains interfaces and classes for manipulating Java beans.
- * It is used by most other Spring packages.
+ * The core package implementing Spring's lightweight Inversion of Control (IoC) container.
  *
- * <p>A BeanWrapper object may be used to set and get bean properties,
- * singly or in bulk.
+ * <p>Provides an alternative to the Singleton and Prototype design
+ * patterns, including a consistent approach to configuration management.
+ * Builds on the org.springframework.beans package.
  *
- * <p>The classes in this package are discussed in Chapter 11 of
+ * <p>This package and related packages are discussed in Chapter 11 of
  * <a href="https://www.amazon.com/exec/obidos/tg/detail/-/0764543857/">Expert One-On-One J2EE Design and Development</a>
  * by Rod Johnson (Wrox, 2002).
  */
 @NonNullApi
 @NonNullFields
-package org.springframework.beans;
+package org.springframework.beans.factory;
 
 import org.springframework.lang.NonNullApi;
 import org.springframework.lang.NonNullFields;
