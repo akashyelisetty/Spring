2f4f200804fa3a8422be140b7d63793df89b8f58
akashyelisetty <43619353+akashyelisetty@users.noreply.github.com>
2019-06-19 09:46:11 -0400

    Add files via upload

--- support/package-info.java
+++ support/package-info.java
@@ -1,10 +1,10 @@
 /**
- * Classes supporting the org.springframework.beans package,
- * such as utility classes for sorting and holding lists of beans.
+ * Classes supporting the {@code org.springframework.beans.factory} package.
+ * Contains abstract base classes for {@code BeanFactory} implementations.
  */
 @NonNullApi
 @NonNullFields
-package org.springframework.beans.support;
+package org.springframework.beans.factory.support;
 
 import org.springframework.lang.NonNullApi;
 import org.springframework.lang.NonNullFields;
